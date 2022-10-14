8a8fbece23bbebf54239bc2b7048f960b01f2372
Numbers00 <kmaquino@up.edu.ph>
2022-10-14 16:33:19 +0800

    finished sliding functionality of the top part of the dashboard

---
+++ kardex_app/static/js/dashboard.js
@@ -0,0 +1,12 @@
+const vizSlider = tns({
+  container: '.viz-slider',
+  items: 4,
+  slideBy: 'page',
+  center: true,
+  mouseDrag: true,
+  swipeAngle: false,
+  controls: false,
+  nav: false,
+  loop: true,
+  speed: 400
+});
